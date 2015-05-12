4f95c56c64a28613c39fc524bdf482cdd153f3bc
MakSim345 <senishch@gmail.com>
2015-05-12 09:20:06 +0300

    update Thread_hasher for new Qt IDE

--- Thread_Hasher/main.cpp
+++ Thread_Hasher/main.cpp
@@ -1,15 +1,12 @@
-// #include <QApplication>
 #include <QtGui>
 #include <QtCore>
 #include <stdio.h>
-// #include "MainWidget.h"
 #include "HasherWidget.h"
 
 int main (int argc, char ** argv)
 {
     QApplication app (argc, argv);
-    QWidget *mainWidget = new HasherWidget();
-    //QWidget *mainWidget = new MainWidget();
+    QWidget *mainWidget = new HasherWidget();    
     mainWidget->setMinimumSize(500, 150);
     mainWidget->show();
     return app.exec();
